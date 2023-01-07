08128091307ce49cac827dd39b016a0cf05ec471
quentuez <dmitrij.klimenkoff7@gmail.com>
2023-01-07 23:41:00 +0700

    Added NavigationMenu.

--- src/components/NavigationMenu/NavigationMenu.jsx
+++ src/components/NavigationMenu/NavigationMenu.jsx
@@ -1,146 +1,52 @@
 import React from "react";
 
 export default function NavigationMenu() {
-  const ECommerce = {
-    Dashboard: "Dashboard",
-    Orders: {
-      List: "List",
-      Detail: "Detail",
-    },
-    Products: {
-      ListView: "List View",
-      GridView: "Grid View",
-      ProductDetail: "Product Detail",
-      ShoppingCart: "Shopping Cart",
-      Checkout: "Checkout",
-    },
-    Buyer: {
-      Dashboard: "Dashboard",
-      Orders: "Orders",
-      Addresses: "Addresses",
-      Wishlist: "Wishlist",
-    },
-    Customers: "Customers",
-    Invoices: {
-      List: "List",
-      Detail: "Detail",
-    },
-  };
+  const ECommerce = [
+    "Dashboard",
+    "Orders",
+    "Products",
+    "Buyer",
+    "Customers",
+    "Invoices",
+  ];
+  const Apps = ["Chats", "Email", "Todo App"];
+  const Pages = [
+    "Profile",
+    "Users",
+    "Authentication",
+    "Error Pages",
+    "Settings",
+    "Pricing Table",
+    "Search Page",
+    "FAQ",
+  ];
+  const UserInterface = [
+    "Components",
+    "Forms",
+    "Content",
+    "Charts",
+    "Extensions",
+  ];
+  const Other = ["Menu Item", "Disabled"];
 
-  const Apps = {
-    Chats: "Chats",
-    Email: {
-      Inbox: "Inbox",
-      Detail: "Detail",
-      EmailTemplate: "Email Template",
-    },
-    TodoApp: {
-      List: "List",
-      Details: "Details",
-    },
-  };
-
-  const Pages = {
-    Profile: {
-      Post: "Post",
-      Connections: "Connections",
-    },
-    Users: {
-      ListView: "List View",
-      GridView: "Grid View",
-    },
-    Authentication: {
-      Login: "Login",
-      Register: "Register",
-      ResetPassword: "Reset Password",
-      LockScreen: "Lock Screen",
-      AccountVerified: "Account Verified",
-    },
-    ErrorPages: {
-      404: "404",
-      AccessDenied: "Access Denied",
-      UnderConstruction: "Under Construction",
-    },
-    Settings: "Settings",
-    PricingTable: "Pricing Table",
-    SearchPage: "Search Page",
-    FAQ: "FAQ",
-  };
-
-  const UserInterface = {
-    Components: {
-      Accordion: "Accordion",
-      Alerts: "Alerts",
-      Badge: "Badge",
-      Breadcrumb: "Breadcrumb",
-      Buttons: "Buttons",
-      ButtonGroup: "Button Group",
-      Card: "Card",
-      CardMasonry: "Card Masonry",
-      Carousel: "Carousel",
-      Collapse: "Collapse",
-      Dropdowns: "Dropdowns",
-      ListGroup: "List Group",
-      Modal: "Modal",
-      NavsAndTab: "Navs and Tabs",
-      Pagination: "Pagination",
-      Popovers: "Popovers",
-      Progress: "Progress",
-      Spinners: "Spinners",
-      Toasts: "Toasts",
-      Tables: "Tables",
-      Tooltip: "Tooltip",
-    },
-    Forms: {
-      FormElements: "Form Elements",
-      Wizard: "Wizard",
-      Repeater: "Repeater",
-      FileUpload: "File Upload",
-      CKEditor: "CKEditor",
-      RangeSlider: "Range Slider",
-      Select2: "Select2",
-      TagsInput: "Tags Input",
-      InputMask: "Input Mask",
-      Datepicker: "Datepicker",
-      ClockPicker: "Clock Picker",
-    },
-    Content: {
-      Typography: "Typography",
-      Images: "Images",
-      Figures: "Figures",
-      Avatar: "Avatar",
-      Icons: "Icons",
-      Colors: "Colors",
-    },
-    Charts: {
-      ApexChart: "Apex Chart",
-      Chartjs: "Chartjs",
-      Justgage: "Justgage",
-      Morsis: "Morsis",
-    },
-    Extensions: {
-      VectorMap: "Vector Map",
-      Datatable: "Datatable",
-      SweetAlert: "Sweet Alert",
-      Lightbox: "Lightbox",
-      Introjs: "Introjs",
-      Nestable: "Nestable",
-      Rating: "Rating",
-      CodeHighlighter: "Code Highlighter",
-    },
-  };
-
-  const Other = {
-    MenuItem: {
-      MenuItem1: "Menu Item 1",
-      MenuItem2: "Menu Item 2",
-    },
-    Disabled: "Disabled",
-  };
+  const ECommerceListItem = ECommerce.map((item) => <li>{item}</li>);
+  const AppsListItem = Apps.map((item) => <li>{item}</li>);
+  const PagesListItem = Pages.map((item) => <li>{item}</li>);
+  const UserInterfaceListItem = UserInterface.map((item) => <li>{item}</li>);
+  const OtherListItem = Other.map((item) => <li>{item}</li>);
 
   return (
-    <ul>
-      <li></li>
-    </ul>
+    <div>
+      <h2>E-Commerce</h2>
+      <ul>{ECommerceListItem}</ul>
+      <h2>Apps</h2>
+      <ul>{AppsListItem}</ul>
+      <h2>Pages</h2>
+      <ul>{PagesListItem}</ul>
+      <h2>User Interface</h2>
+      <ul>{UserInterfaceListItem}</ul>
+      <h2>Other</h2>
+      <ul>{OtherListItem}</ul>
+    </div>
   );
 }
